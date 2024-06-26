76ac2ee7bf42983021e8ee9aa8e9ecaed068c6ab
Bambam <bambam.pataranan@gmail.com>
2024-06-27 01:31:08 +0700

    Q3.6

--- main.js
+++ main.js
@@ -6,10 +6,12 @@ createApp ({
         const product = ref('Boots')
         const description = ref('Size 36-45')
         const image = ref('./assets/images/socks_green.jpg')
+        const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         return {
             product , 
             description ,
-            image
+            image ,
+            link
         }
     }
 }) .mount('#app')

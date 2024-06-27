2236c03f10377a5a5d244facad1166c8c516bb83
Bambam <bambam.pataranan@gmail.com>
2024-06-27 23:32:57 +0700

    Q5.5

--- main.js
+++ main.js
@@ -19,6 +19,9 @@ createApp ({
             {id: 2234 , color: 'green'} ,
             {id: 2235 , color: 'blue'}
         ])
+        const sizes = ref([
+            'S  ' , 'M  ' , 'L  '
+        ])
 
         return {
             product , 
@@ -29,7 +32,8 @@ createApp ({
             inventory ,
             onSale ,
             details ,
-            variants
+            variants ,
+            sizes
         }
     }
 }) .mount('#app')
